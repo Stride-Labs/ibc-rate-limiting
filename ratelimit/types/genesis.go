71a0764e584c216968d078df36ebccf353daa722
sampocs <sam.pochyly@gmail.com>
2024-02-16 10:38:23 -0600

    added hour epoch to genesis type

--- ratelimit/types/genesis.go
+++ ratelimit/types/genesis.go
@@ -1,10 +1,19 @@
 package types
 
+import time "time"
+
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		Params:     DefaultParams(),
-		RateLimits: []RateLimit{},
+		Params:                           DefaultParams(),
+		RateLimits:                       []RateLimit{},
+		WhitelistedAddressPairs:          []WhitelistedAddressPair{},
+		BlacklistedDenoms:                []string{},
+		PendingSendPacketSequenceNumbers: []string{},
+		HourEpoch: HourEpoch{
+			EpochNumber: 0,
+			Duration:    time.Hour,
+		},
 	}
 }
 
