6b81c42e0594c8f38b454bb8adac07eaca6e7bbb
sampocs <sam.pochyly@gmail.com>
2024-02-21 11:39:23 -0600

    added authority to keeper and removed gov handlers

--- ratelimit/keeper/keeper.go
+++ ratelimit/keeper/keeper.go
@@ -18,6 +18,7 @@ type (
 		storeKey   storetypes.StoreKey
 		cdc        codec.BinaryCodec
 		paramstore paramtypes.Subspace
+		authority  string
 
 		bankKeeper    types.BankKeeper
 		channelKeeper types.ChannelKeeper
@@ -29,6 +30,7 @@ func NewKeeper(
 	cdc codec.BinaryCodec,
 	key storetypes.StoreKey,
 	ps paramtypes.Subspace,
+	authority string,
 	bankKeeper types.BankKeeper,
 	channelKeeper types.ChannelKeeper,
 	ics4Wrapper types.ICS4Wrapper,
@@ -37,6 +39,7 @@ func NewKeeper(
 		cdc:           cdc,
 		storeKey:      key,
 		paramstore:    ps,
+		authority:     authority,
 		bankKeeper:    bankKeeper,
 		channelKeeper: channelKeeper,
 		ics4Wrapper:   ics4Wrapper,
