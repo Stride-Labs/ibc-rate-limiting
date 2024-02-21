d5d76d67322d796d222192f84a6d470244bf3aa2
sampocs <sam.pochyly@gmail.com>
2024-02-21 11:59:12 -0600

    implemented msg_server

--- ratelimit/keeper/msg_server.go
+++ ratelimit/keeper/msg_server.go
@@ -1,44 +1,62 @@
-package gov
+package keeper
 
 import (
+	"context"
+
+	errorsmod "cosmossdk.io/errors"
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
-	channelkeeper "github.com/cosmos/ibc-go/v7/modules/core/04-channel/keeper"
 
-	"github.com/Stride-Labs/ibc-rate-limiting/ratelimit/keeper"
 	"github.com/Stride-Labs/ibc-rate-limiting/ratelimit/types"
 )
 
+type msgServer struct {
+	Keeper
+}
+
+// NewMsgServerImpl returns an implementation of the ratelimit MsgServer interface
+func NewMsgServerImpl(keeper Keeper) types.MsgServer {
+	return &msgServer{Keeper: keeper}
+}
+
+var _ types.MsgServer = msgServer{}
+
 // Adds a new rate limit. Fails if the rate limit already exists or the channel value is 0
-func AddRateLimit(ctx sdk.Context, k keeper.Keeper, channelKeeper channelkeeper.Keeper, p *types.AddRateLimitProposal) error {
+func (k msgServer) AddRateLimit(goCtx context.Context, msg *types.MsgAddRateLimit) (*types.MsgAddRateLimitResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if k.authority != msg.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
 	// Confirm the channel value is not zero
-	channelValue := k.GetChannelValue(ctx, p.Denom)
+	channelValue := k.Keeper.GetChannelValue(ctx, msg.Denom)
 	if channelValue.IsZero() {
-		return types.ErrZeroChannelValue
+		return nil, types.ErrZeroChannelValue
 	}
 
 	// Confirm the rate limit does not already exist
-	_, found := k.GetRateLimit(ctx, p.Denom, p.ChannelId)
+	_, found := k.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
 	if found {
-		return types.ErrRateLimitAlreadyExists
+		return nil, types.ErrRateLimitAlreadyExists
 	}
 
 	// Confirm the channel exists
-	_, found = channelKeeper.GetChannel(ctx, transfertypes.PortID, p.ChannelId)
+	_, found = k.Keeper.channelKeeper.GetChannel(ctx, transfertypes.PortID, msg.ChannelId)
 	if !found {
-		return types.ErrChannelNotFound
+		return nil, types.ErrChannelNotFound
 	}
 
 	// Create and store the rate limit object
 	path := types.Path{
-		Denom:     p.Denom,
-		ChannelId: p.ChannelId,
+		Denom:     msg.Denom,
+		ChannelId: msg.ChannelId,
 	}
 	quota := types.Quota{
-		MaxPercentSend: p.MaxPercentSend,
-		MaxPercentRecv: p.MaxPercentRecv,
-		DurationHours:  p.DurationHours,
+		MaxPercentSend: msg.MaxPercentSend,
+		MaxPercentRecv: msg.MaxPercentRecv,
+		DurationHours:  msg.DurationHours,
 	}
 	flow := types.Flow{
 		Inflow:       sdkmath.ZeroInt(),
@@ -46,61 +64,80 @@ func AddRateLimit(ctx sdk.Context, k keeper.Keeper, channelKeeper channelkeeper.
 		ChannelValue: channelValue,
 	}
 
-	k.SetRateLimit(ctx, types.RateLimit{
+	k.Keeper.SetRateLimit(ctx, types.RateLimit{
 		Path:  &path,
 		Quota: &quota,
 		Flow:  &flow,
 	})
 
-	return nil
+	return &types.MsgAddRateLimitResponse{}, nil
 }
 
 // Updates an existing rate limit. Fails if the rate limit doesn't exist
-func UpdateRateLimit(ctx sdk.Context, k keeper.Keeper, p *types.UpdateRateLimitProposal) error {
+func (k msgServer) UpdateRateLimit(goCtx context.Context, msg *types.MsgUpdateRateLimit) (*types.MsgUpdateRateLimitResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if k.authority != msg.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
 	// Confirm the rate limit exists
-	_, found := k.GetRateLimit(ctx, p.Denom, p.ChannelId)
+	_, found := k.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
 	if !found {
-		return types.ErrRateLimitNotFound
+		return nil, types.ErrRateLimitNotFound
 	}
 
 	// Update the rate limit object with the new quota information
 	// The flow should also get reset to 0
 	path := types.Path{
-		Denom:     p.Denom,
-		ChannelId: p.ChannelId,
+		Denom:     msg.Denom,
+		ChannelId: msg.ChannelId,
 	}
 	quota := types.Quota{
-		MaxPercentSend: p.MaxPercentSend,
-		MaxPercentRecv: p.MaxPercentRecv,
-		DurationHours:  p.DurationHours,
+		MaxPercentSend: msg.MaxPercentSend,
+		MaxPercentRecv: msg.MaxPercentRecv,
+		DurationHours:  msg.DurationHours,
 	}
 	flow := types.Flow{
 		Inflow:       sdkmath.ZeroInt(),
 		Outflow:      sdkmath.ZeroInt(),
-		ChannelValue: k.GetChannelValue(ctx, p.Denom),
+		ChannelValue: k.GetChannelValue(ctx, msg.Denom),
 	}
 
-	k.SetRateLimit(ctx, types.RateLimit{
+	k.Keeper.SetRateLimit(ctx, types.RateLimit{
 		Path:  &path,
 		Quota: &quota,
 		Flow:  &flow,
 	})
 
-	return nil
+	return &types.MsgUpdateRateLimitResponse{}, nil
 }
 
 // Removes a rate limit. Fails if the rate limit doesn't exist
-func RemoveRateLimit(ctx sdk.Context, k keeper.Keeper, msg *types.RemoveRateLimitProposal) error {
-	_, found := k.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
-	if !found {
-		return types.ErrRateLimitNotFound
+func (k msgServer) RemoveRateLimit(goCtx context.Context, msg *types.MsgRemoveRateLimit) (*types.MsgRemoveRateLimitResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if k.authority != msg.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
 	}
 
-	k.RemoveRateLimit(ctx, msg.Denom, msg.ChannelId)
-	return nil
+	_, found := k.Keeper.GetRateLimit(ctx, msg.Denom, msg.ChannelId)
+	if !found {
+		return nil, types.ErrRateLimitNotFound
+	}
+
+	k.Keeper.RemoveRateLimit(ctx, msg.Denom, msg.ChannelId)
+	return &types.MsgRemoveRateLimitResponse{}, nil
 }
 
 // Resets the flow on a rate limit. Fails if the rate limit doesn't exist
-func ResetRateLimit(ctx sdk.Context, k keeper.Keeper, msg *types.ResetRateLimitProposal) error {
-	return k.ResetRateLimit(ctx, msg.Denom, msg.ChannelId)
+func (k msgServer) ResetRateLimit(goCtx context.Context, msg *types.MsgResetRateLimit) (*types.MsgResetRateLimitResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if k.authority != msg.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, msg.Authority)
+	}
+
+	if err := k.Keeper.ResetRateLimit(ctx, msg.Denom, msg.ChannelId); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgResetRateLimitResponse{}, nil
 }
